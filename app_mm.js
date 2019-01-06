b78965d8006561ba37eea1a859ecbf2dbf75a40c
marleigh <volcana@gmail.com>
2019-01-05 16:22:59 -0800

    basic JS done

--- app_mm.js
+++ app_mm.js
@@ -20,6 +20,30 @@ function addTask(e){
         alert("Add a task");
     }
 
+    //create li element
+    const li = document.createElement('li');
+    
+    //add class
+    li.className = 'collection-item';
+
+    //create text node
+    li.appendChild(document.createTextNode(taskInput.value));
+
+    // create new link element
+    const link = document.createElement('a');
+    link.className = 'delete-item secondary-content';
+
+    //add icon html
+
+    link.innerHTML = '<i class="fa fa-remove"></i>';
+    li.appendChild(link);
+
+    //append li to the ul
+    taskList.appendChild(li);
+
+    //clear input
+    taskInput.value = '';
+
     //prevent default behavior - submit event
     e.preventDefault();
 }
