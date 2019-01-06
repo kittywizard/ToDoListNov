e5f88c5e753ce5e4e53f5688c163611a0ccc0f3d
marleigh <volcana@gmail.com>
2019-01-05 16:03:41 -0800

    ..

--- app_mm.js
+++ app_mm.js
@@ -5,3 +5,21 @@ const taskList = document.querySelector('.collection');
 const clearBtn = document.querySelector('.clear-tasks');
 const filter = document.querySelector('#filter');
 const taskInput = document.querySelector('#task');
+
+//Loaf all event listeners
+
+loadEventListeners();
+
+function loadEventListeners() {
+    //add task event
+    form.addEventListener('submit', addTask);
+}
+
+function addTask(e){
+    if(taskInput.value === ''){
+        alert("Add a task");
+    }
+
+    //prevent default behavior - submit event
+    e.preventDefault();
+}
