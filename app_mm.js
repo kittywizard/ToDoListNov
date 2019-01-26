5ee4409b4076aa3b2a76f397cd1f3adaf883522b
marleigh <volcana@gmail.com>
2019-01-26 14:23:39 -0800

    UI Complete

--- app_mm.js
+++ app_mm.js
@@ -15,6 +15,10 @@ function loadEventListeners() {
     form.addEventListener('submit', addTask);
     // remove task event
     taskList.addEventListener('click', removeTask);
+    //Clear Tasks
+    clearBtn.addEventListener('click', clearTasks);
+    //Filter Tasks
+    filter.addEventListener('keyup', filterTasks);
 }
 
 function addTask(e){
@@ -52,7 +56,37 @@ function addTask(e){
 
 function removeTask(e) {
     if(e.target.parentElement.classList.contains('delete-item')) {
-        console.log(e.target);
+        if(confirm("Are you sure?")){
+            e.target.parentElement.parentElement.remove();
 
+        }
     }
+} 
+
+function clearTasks() {
+    //taskList.innerHTML = '';
+
+    //Faster Way
+    while (taskList.firstChild) {
+        taskList.removeChild(taskList.firstChild);
+    }
+
+}
+
+function filterTasks(e) {
+    const text = e.target.value.toLowerCase();
+    
+    //can use forEach because using querySelectorAll returns a node list 
+    document.querySelectorAll('.collection-item').forEach(
+        function(task){
+            const item = task.firstChild.textContent;
+
+            if(item.toLowerCase().indexOf(text) != -1){
+                    task.style.display = 'block';
+            }
+            else {
+                task.style.display = 'none';
+            }
+        }
+    );
 }
